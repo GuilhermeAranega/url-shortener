68a00a36ae97ed4db0c541aa362c4c8ab5807cc3
Guilherme Aranega <guilhermee.aranega@gmail.com>
2023-08-09 22:36:53 -0300

    started to create the app ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import './globals.css'
+import Header from './components/Header'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Url Shortener',
+  description: 'Shorten links',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <header>
+          <Header />
+        </header>
+        <main> { children }</main>
+      </body>
+    </html>
+  )
+}
+
+// e oq tava antes no return do RootLayout
+// <body> className={inter.className}>{children} </body>
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <div className="flex flex-col items-center justify-center">
+        <p className='text-5xl font-bold hover:italic'>Encurte seus links loooooongos</p>
+      </div>
+      
+    </main>
+  )
+}
